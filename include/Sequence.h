4802329dc4859dafffb1bde9edeb94e5a48fe457
SKILL324 <93397324+SKILL324@users.noreply.github.com>
2022-02-10 11:31:56 -0300

    Add files via upload

---
+++ include/Sequence.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "Sequence.h"
+#define SEQUENCE_SIZE 50
+#define NUMBER '0'
+#define LOWERCASE '1'
+#define UPPERCASE '2'
+
+enum CASE 
+{
+	Lower, Upper, BothCase
+};
+
+enum Textpos
+{
+	Start = 1, End, BothPos
+};
+
+void Sort_Words(FILE* hFile, int number_words, int sequence_size);
+void Sequence_Conversion(char* sequence, char* buffer);
+void Sequence_Loop(char* sequence);
+int Sequence_Validate(char* sequence);
+void rndCharacter(char* rdChar, unsigned int case_type);
+void rndNumber(char* rdChar);
+FILE* CreateFile(const char* path, const char* mode);
+int FileWriteLine(char* buffer, FILE* hFile);
+int GenerateKeys();
+int EditFileKeys();
+int FileReadLine(const char* path, char* buffer, FILE* hFile);
+void ConcatenateText(char* text_edited, char* string1, char* string2, char* string3);
